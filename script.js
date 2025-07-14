0d4cc81c4dd9ecc9bd9b0cf6a616d51d4e8e6191
matheusmachado-dev <matheusmc.dev@gmail.com>
2025-07-13 22:35:07 -0300

    feat: add `playRound` function

--- script.js
+++ script.js
@@ -1,20 +1,56 @@
 let humanScore = 0;
 let computerScore = 0;
 
+let humanChoice;
+
+console.log("Computer Score: " + computerScore);
+console.log("Human Score: " + humanScore);
+
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3);
   if (randomNumber === 0) {
-    return "Rock!";
+    return "rock";
   }
   if (randomNumber === 1) {
-    return "Paper!";
+    return "paper";
   }
-  return "Scissors!";
+  return "scissors";
 }
 
 function getHumanChoice() {
   return prompt("Choose between rock, paper, or scissors");
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+  const sanitizedHumanChoice = sanitizeHumanChoice(humanChoice);
+
+  if (sanitizedHumanChoice === "rock" && computerChoice === "scissors") {
+    humanScore++;
+    return console.log("You Won! " + humanChoice + " beats " + computerChoice);
+  }
+  if (sanitizedHumanChoice === "scissors" && computerChoice === "paper") {
+    humanScore++;
+    return console.log("You Won! " + humanChoice + " beats " + computerChoice);
+  }
+  if (sanitizedHumanChoice === "paper" && computerChoice === "rock") {
+    humanScore++;
+    return console.log("You Won! " + humanChoice + " beats " + computerChoice);
+  }
+  if (sanitizedHumanChoice === computerChoice) {
+    return console.log("Draw! Nobody won");
+  }
+  computerScore++;
+  return console.log("You Lost! \n" + computerChoice + " beats " + humanChoice);
+}
+
+function sanitizeHumanChoice(humanChoice) {
+  return humanChoice.toLowerCase();
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+console.log("Computer Score: " + computerScore);
+console.log("Human Score: " + humanScore);
