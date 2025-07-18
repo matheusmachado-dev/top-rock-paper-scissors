b88260cb24c05ba3ae23265a604c0307bcdc9455
matheusmachado-dev <matheusmc.dev@gmail.com>
2025-07-18 14:38:39 -0300

    feat: recreate game logic

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let humanScore = 0;
 let computerScore = 0;
+let round = 1;
 
 console.log("Computer Score: " + computerScore);
 console.log("Human Score: " + humanScore);
@@ -7,9 +8,13 @@ console.log("Human Score: " + humanScore);
 const rockBtn = document.querySelector("#rock-btn");
 const paperBtn = document.querySelector("#paper-btn");
 const scissorsBtn = document.querySelector("#scissors-btn");
+const resetBtn = document.querySelector("#reset-btn");
 
 const humanScoreElement = document.querySelector("#human-score");
 const computerScoreElement = document.querySelector("#computer-score");
+const roundCounter = document.querySelector("#round");
+const containerBoard = document.querySelector("#container-board");
+const resultsBoard = document.querySelector("#results-board");
 
 rockBtn.addEventListener("click", (e) => {
   const computerChoice = getComputerChoice();
@@ -23,6 +28,7 @@ scissorsBtn.addEventListener("click", (e) => {
   const computerChoice = getComputerChoice();
   playRound("scissors", computerChoice);
 });
+resetBtn.addEventListener("click", resetGame);
 
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3);
@@ -44,61 +50,85 @@ function playRound(humanChoice, computerChoice) {
   if (sanitizedHumanChoice === "rock" && computerChoice === "scissors") {
     humanScore++;
     humanScoreElement.textContent = "You: " + humanScore;
+    showResults("Player won!", round, sanitizedHumanChoice, computerChoice);
+    round++;
+    roundCounter.textContent = `Round: ${round}`;
     getWinner();
     return;
   }
   if (sanitizedHumanChoice === "scissors" && computerChoice === "paper") {
     humanScore++;
     humanScoreElement.textContent = "You: " + humanScore;
+    showResults("Player won!", round, sanitizedHumanChoice, computerChoice);
+    round++;
+    roundCounter.textContent = `Round: ${round}`;
     getWinner();
     return;
   }
   if (sanitizedHumanChoice === "paper" && computerChoice === "rock") {
     humanScore++;
     humanScoreElement.textContent = "You: " + humanScore;
+    showResults("Player won!", round, sanitizedHumanChoice, computerChoice);
+    round++;
+    roundCounter.textContent = `Round: ${round}`;
     getWinner();
     return;
   }
   if (sanitizedHumanChoice === computerChoice) {
+    showResults("Draw!", round, sanitizedHumanChoice, computerChoice, true);
+    round++;
+    roundCounter.textContent = `Round: ${round}`;
     return console.log("Draw!\nNobody won the round");
   }
   computerScore++;
   computerScoreElement.textContent = "Computer: " + computerScore;
+  showResults("Computer won!", round, computerChoice, sanitizedHumanChoice);
+  round++;
+  roundCounter.textContent = `Round: ${round}`;
   getWinner();
   return;
 }
 
 function getWinner() {
   if (humanScore === 5) {
-    console.log("Player won!");
+    containerBoard.classList.toggle("disabled");
+    resetBtn.classList.toggle("disabled");
+    return alert("You win!");
   }
   if (computerScore === 5) {
-    console.log("Computer won!");
+    containerBoard.classList.toggle("disabled");
+    resetBtn.classList.toggle("disabled");
+    return alert("You lost!");
   }
+}
+
+function resetGame() {
+  humanScore = 0;
+  computerScore = 0;
+  round = 1;
+
+  humanScoreElement.textContent = "You: " + humanScore;
+  computerScoreElement.textContent = "Computer: " + computerScore;
+  roundCounter.textContent = `Round: ${round}`;
+  containerBoard.classList.toggle("disabled");
+  resultsBoard.textContent = "";
+  resetBtn.classList.toggle("disabled");
+}
+
+function showResults(winner, round, winnerChoice, loserChoice, isDraw = false) {
+  const roundResult = document.createElement("p");
+  if (isDraw) {
+    roundResult.textContent = `Round ${round}: Draw! both players chose ${winnerChoice}`;
+    resultsBoard.appendChild(roundResult);
+    return;
+  }
+  roundResult.textContent = `Round ${round}: ${winner} ${winnerChoice} beats ${loserChoice}`;
+  resultsBoard.appendChild(roundResult);
+  return;
 }
 
 function sanitizeHumanChoice(humanChoice) {
   return humanChoice.toLowerCase();
 }
 
-function playGame() {
-  for (let round = 0; round < 5; round++) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-
-    console.log("Round " + (round + 1) + " results:");
-
-    playRound(humanSelection, computerSelection);
-
-    console.log("Computer Score: " + computerScore);
-    console.log("Human Score: " + humanScore);
-  }
-  if (humanScore > computerScore) {
-    return console.log("You won the game!");
-  }
-  if (computerScore > humanScore) {
-    return console.log("The computer won the game!");
-  }
-
-  return console.log("Nobody won the game!");
-}
+function playGame() {}
